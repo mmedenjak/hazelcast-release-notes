53a6182e11d06ddc0f2b0dd19d28fef8d5fd9a80
Serdar Ozmen <serdar@hazelcast.com>
2017-05-09 08:16:50 +0300

    Updated release notes.

---
+++ target/html-single/assets/js/scrollUp.js
@@ -0,0 +1,12 @@
+	$(function () {
+		$.scrollUp({
+			scrollName: 'scrollUp',
+			topDistance: '300',
+			topSpeed: 100,
+			animation: 'fade',
+			animationInSpeed: 100,
+			animationOutSpeed: 100,
+			scrollText: 'Back to Top',
+			activeOverlay: false,
+		});
+	});
